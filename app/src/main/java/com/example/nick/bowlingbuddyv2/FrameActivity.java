f26143e2355e7810c6b24daf27c5cd87eda6aacc
Nicholas A. Grace <graceni360@gmail.com>
2017-05-16 13:50:05 -0400

    COMMIT ALREADY

--- app/src/main/java/com/example/nick/bowlingbuddyv2/FrameActivity.java
+++ app/src/main/java/com/example/nick/bowlingbuddyv2/FrameActivity.java
@@ -5,7 +5,7 @@ import android.os.Bundle;
 
 public class FrameActivity extends AppCompatActivity {
 
-    boolean ex;
+
 ///////
     @Override
     protected void onCreate(Bundle savedInstanceState) {
